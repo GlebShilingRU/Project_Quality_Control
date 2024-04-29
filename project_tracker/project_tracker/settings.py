4d8f7804ba3a5d1db62b5a5ef5fa02786e50e33a
Shiling Gleb <shgn22m633@student.bmstu.ru>
2024-04-29 15:53:06 +0300

    Добавлена стилизация формы project_create

--- project_tracker/project_tracker/settings.py
+++ project_tracker/project_tracker/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tasks.apps.TasksConfig',
     'quality_control.apps.QualityControlConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
